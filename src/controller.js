b4b77abd33f9e5a1d806379dd2acdc64f3e7cbcf
geroserial <geronimoserial@gmail.com>
2023-10-18 22:50:09 -0700

    Libros

--- src/controller.js
+++ src/controller.js
@@ -28,13 +28,13 @@ class LibroController {
 
      async delete(req,res){
         const libro = req.body;
-        const [result] = await pool.query(`DELETE FROM Personas WHERE id=(?)`, [libro.id]);
+        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`, [libro.id]);
         res.json({"Registros eliminados": result.affectedRows});
     }
 
     async update(req,res){
         const libro = req.body;
-        const [result] = await pool.query(`UPDATE Personas SET nombre=(?), autor=(?), categoria=(?), añoPublicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.añoPublicacion, libro.ISBN, libro.id]);
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), añoPublicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.añoPublicacion, libro.ISBN, libro.id]);
         res.json({"Registros actualizados": result.changedRows});
     }
    
